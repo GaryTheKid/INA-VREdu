7b719080a9b5ef045a05963b9c61697b9876c5c4
MeanStudent <kh3074@cumc.columbia.edu>
2022-04-17 16:18:22 -0400

    vector visualization

---
+++ Assets/Scripts/Player/SpeedVector.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedVector : MonoBehaviour
+{
+    public GameObject speedVectorPrefab;
+    public GameObject parentObject;
+    public Rigidbody parentRB;
+    public Vector3 parentSpeed;
+    public Material vectorMaterial;
+
+    public float colorScale;
+    public float emissionIntensityScale;
+    public float lengthScale;
+
+    private void Start()
+    {
+        parentRB = parentObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        parentSpeed = GetSpeedVector();
+        UpdateVector();
+    }
+
+    private Vector3 GetSpeedVector()
+    {
+        return parentRB.velocity;
+    }
+
+    private void UpdateVector()
+    {
+        // update color
+        float speedMode = parentSpeed.x;
+        float gb = speedMode * colorScale;
+        float r = 255f - gb;
+        float intensity = speedMode * emissionIntensityScale;
+        Color currentColor = new Color(r, gb, gb);
+        vectorMaterial.SetColor("_EmissionColor",currentColor*intensity);
+
+        // update length
+        speedVectorPrefab.transform.localScale = 
+            new Vector3(speedVectorPrefab.transform.localScale.x,speedMode,
+                speedVectorPrefab.transform.localScale.z);
+        
+    }
+    
+    
+    
+}
